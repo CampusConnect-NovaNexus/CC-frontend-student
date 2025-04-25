7648f0783ba488507d800b1cae75479365d4e582
Charity Rymbai <charityrymbai@hotmail.com>
2025-04-26 00:58:11 +0530

    deleted redundant code files

--- service/lost-found/postFoundItem.ts
+++
@@ -1,6 +0,0 @@
-import { LostFoundItemInput, prepareItemFormData, submitItemToAPI } from '../../utils/lostFoundHelper';
-
-export const postFoundItem = async (item: LostFoundItemInput) => {
-  const formData = await prepareItemFormData(item, 0.1, 500);
-  return submitItemToAPI(formData, 'found');
-};
--- service/lost-found/postLostItem.ts
+++
@@ -1,6 +0,0 @@
-import { LostFoundItemInput, prepareItemFormData, submitItemToAPI } from '../../utils/lostFoundHelper';
-
-export const postLostItem = async (item: LostFoundItemInput) => {
-  const formData = await prepareItemFormData(item, 0.1, 500);
-  return submitItemToAPI(formData, 'lost');
-};
